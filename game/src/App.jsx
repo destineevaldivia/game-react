ee1b3b80668d4a14c67609dab582f84e8a71fad5
destineevaldivia <destvaldivia@gmail.com>
2023-08-17 21:52:41 -0700

    added game tags and buttons

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,27 +7,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1> Rock Paper Scissors Game </h1>
+    </div>
+
+    <h2>Choose:</h2>
+        <button class="buttons" id="rock">Rock</button>
+        <button class="buttons" id="paper">Paper</button>
+        <button class="buttons" id="scissors">Scissors</button>
+
+    <div> 
+        <h2> Your Choice: <span id = "your-choice"></span></h2>
+        <h2> Computer Choice: <span id="computer-choice"></span></h2>
+        <h2> Result: <span id = "result"></span></h2>
+    </div>
+
     </>
   )
 }
 
-export default App
+export default App;

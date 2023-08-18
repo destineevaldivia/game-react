78de58d96c97982d6752dcd90a8a0c801569b01e
destineevaldivia <destvaldivia@gmail.com>
2023-08-18 11:15:26 -0700

    imported font awesome and added the icons to buttons

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandBackFist, faHand, faHandScissors } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,9 +14,9 @@ function App() {
 
     <div className="choice-container">
         <h2>Choose:</h2>
-        <button className="buttons" id="rock-btn">Rock</button>
-        <button className="buttons" id="paper-btn">Paper</button>
-        <button className="buttons" id="scissors-btn">Scissors</button>
+        <button className="buttons" id="rock-btn">Rock <FontAwesomeIcon icon={faHandBackFist} /></button>
+        <button className="buttons" id="paper-btn">Paper <FontAwesomeIcon icon={faHand} /></button>
+        <button className="buttons" id="scissors-btn">Scissors <FontAwesomeIcon icon={faHandScissors} /></button>
     </div>
 
     <div className="output-container"> 

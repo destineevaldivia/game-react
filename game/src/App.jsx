7101ef4acc223eef080070776cfa16c0bdb37611
destineevaldivia <destvaldivia@gmail.com>
2023-08-18 10:50:05 -0700

    finished basic layout css

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -6,19 +6,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1> Rock Paper Scissors Game </h1>
+    <div className ="body">
+      <h1> Rock Paper Scissors Game </h1>
     </div>
 
-    <h2>Choose:</h2>
-        <button class="buttons" id="rock">Rock</button>
-        <button class="buttons" id="paper">Paper</button>
-        <button class="buttons" id="scissors">Scissors</button>
+    <div className="choice-container">
+        <h2>Choose:</h2>
+        <button className="buttons" id="rock-btn">Rock</button>
+        <button className="buttons" id="paper-btn">Paper</button>
+        <button className="buttons" id="scissors-btn">Scissors</button>
+    </div>
 
-    <div> 
-        <h2> Your Choice: <span id = "your-choice"></span></h2>
-        <h2> Computer Choice: <span id="computer-choice"></span></h2>
-        <h2> Result: <span id = "result"></span></h2>
+    <div className="output-container"> 
+        <h2> Player's Choice: <span id = "player-choice"></span></h2>
+        <h2> Computer's Choice: <span id="computer-choice"></span></h2>
+        <h2> Who won? <span id = "result"></span></h2>
     </div>
 
     </>
